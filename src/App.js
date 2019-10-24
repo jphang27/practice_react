5120027310c17858df59ba318e5480c6090ff62b
John Phang <jphang@talentpath.com>
2019-10-24 15:41:58 -0500

    was able to display car parts on webpage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        Car Parts 
       </header>
+        <CarPartsList>
+          <CParts>keys</CParts>
+          <CParts>engine</CParts>
+          <CParts>headlights</CParts>
+          <CParts>rearlights</CParts>
+          <CParts>wheels</CParts>
+          <CParts>gas</CParts>
+        </CarPartsList>
     </div>
   );
+}
+
+function CarPartsList(props){
+  return <ul>{props.children}</ul>
+}
+function CParts(props){
+  return <li>{props.children}</li>
 }
 
 export default App;
